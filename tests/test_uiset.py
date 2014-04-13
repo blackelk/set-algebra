5eb244bcf4ffc4604739bfd499c9ad50b9577215
Constantine Parkhimovich <cp@core-tech.ru>
2014-04-13 17:04:45 +0200

    Added test for UISet.__inverse__()

--- tests/test_uiset.py
+++ tests/test_uiset.py
@@ -67,3 +67,55 @@ def test_uiset_add():
     assert i3 == Interval('[-1, 0]')
     assert i4 == Interval('(0, 1)')
 
+
+def test_uiset_inverse():
+
+    i0 = Interval('(-inf, inf)')
+    s = UISet([i0])
+    assert (~s).intervals == []
+    assert (~~s).intervals == s.intervals
+
+    s = UISet()
+    assert (~s).intervals == [i0]
+    assert (~~s).intervals == s.intervals
+
+    i1 = Interval('[1, 4]')
+    i2 = Interval('[7, 9]')
+    s = UISet([i1, i2])
+    expected = [Interval('(-inf, 1)'), Interval('(4, 7)'), Interval('(9, inf)')]
+    assert (~s).intervals == expected
+    assert (~~s).intervals == s.intervals
+
+    i3 = Interval('[5, 8]')
+    i4 = Interval('[1, 6]')
+    s = UISet([i3, i4])
+    assert (~s).intervals == [Interval('(-inf, 1)'), Interval('(8, inf)')]
+    assert (~~s).intervals == s.intervals
+
+    i5 = Interval('(0, 1)')
+    i6 = Interval('(1, 2)')
+    s = UISet([i5, i6])
+    expected = [Interval('(-inf, 0]'), Interval('[1, 1]'), Interval('[2, inf)')]
+    assert (~s).intervals == expected
+    assert (~~s).intervals == s.intervals
+
+    i7 = Interval('(-inf, 0)')
+    s = UISet([i7])
+    assert (~s).intervals == [Interval('[0, inf)')]
+    assert (~~s).intervals == s.intervals
+
+    i8 = Interval('[0, inf)')
+    s = UISet([i8])
+    assert (~s).intervals == [i7]
+    assert (~~s).intervals == s.intervals
+
+    assert i0 == Interval('(-inf, inf)')
+    assert i1 == Interval('[1, 4]')
+    assert i2 == Interval('[7, 9]')
+    assert i3 == Interval('[5, 8]')
+    assert i4 == Interval('[1, 6]')
+    assert i5 == Interval('(0, 1)')
+    assert i6 == Interval('(1, 2)')
+    assert i7 == Interval('(-inf, 0)')
+    assert i8 == Interval('[0, inf)')
+
